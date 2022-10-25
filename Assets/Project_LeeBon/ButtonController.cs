9c395f5d3ef2d1686c4e582d49f8e63ffe62da6d
lectures202x <lectures.202x@gmail.com>
2022-10-25 19:42:24 +0900

    20221025

--- Assets/Project_LeeBon/ButtonController.cs
+++ Assets/Project_LeeBon/ButtonController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO.Ports;
+using System;
 
 public class ButtonController : MonoBehaviour
 {
@@ -9,11 +11,26 @@ public class ButtonController : MonoBehaviour
     string InputText;
     public SporeContainerController SporeContainerScript;
 
+    SerialPort Arduino;
+    //string portName = "COM5";
+    string portName = "/dev/cu.usbmodem14201";
+    string SerialIn = null;
+
     void Start()
     {
         InputText = InputField.text;
         InputField.ActivateInputField();
         InputField.Select();
+
+        string[] ports = SerialPort.GetPortNames();
+
+        foreach (string port in ports)
+        {
+            print(port);
+        }
+        Arduino = new SerialPort(portName.ToString(), 9600);
+        Arduino.Open();
+        Arduino.ReadTimeout = 1000; //milliseconds
     }
 
     private void Update()
@@ -22,6 +39,30 @@ public class ButtonController : MonoBehaviour
         {
             Clone();
         }
+
+        if (Arduino.IsOpen)
+        {
+            try
+            {
+                SerialIn = Arduino.ReadLine();
+                print("raw:" + SerialIn);
+                if (SerialIn != null)
+                {
+                    SerialIn = SerialIn.Trim();
+                    print("trimmed:" + SerialIn);
+                    if (SerialIn == "Y")
+                    {
+                        print("got Y");
+                        Clone();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                print(e);
+            }
+        }
+
     }
 
     public void Clone()
@@ -32,5 +73,10 @@ public class ButtonController : MonoBehaviour
         InputField.ActivateInputField();
         InputField.Select();
         SporeContainerScript.GenerateGameObject(InputText);
+    }
+
+    void OnApplicationQuit()
+    {
+        Arduino.Close();
     }
 }
